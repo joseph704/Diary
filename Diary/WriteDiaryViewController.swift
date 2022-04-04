ecf88b544c3cf7c37c102ef6566b2446fcb723be
JosephCha <freelove54@naver.com>
2022-04-04 20:41:15 +0900

    일기 작성 UI 컴포넌트 추가

---
+++ Diary/WriteDiaryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by 차요셉 on 2022/04/04.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
