cdd30754265afc7bf2df90a62939acda25a80af8
JosephCha <freelove54@naver.com>
2022-04-04 20:51:40 +0900

    일기 상세정보 UI 컴포넌트 추가

---
+++ Diary/DiaryDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by 차요셉 on 2022/04/04.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
